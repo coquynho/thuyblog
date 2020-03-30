0d5f98a02c6362c8eaa29783542d803306aa7341
thuy tran <thuy.tran>
2020-03-30 14:33:22 +0700

    add http

---
+++ src/Controller/Blog/CreateBlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Blog;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CreateBlogController extends AbstractController
+{
+    public function __invoke()
+    {
+        
+    }
+}
